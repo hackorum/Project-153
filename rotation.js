37d4b541a7df4cceaae85c858c59d96765f20b39
hackorum <neilsabde@gmail.com>
2021-08-21 13:51:55 +0530

    First Commit

---
+++ rotation.js
@@ -0,0 +1,63 @@
+// AFRAME.registerComponent("scuba-diver-rotation-reader", {
+//   init: function () {
+//     window.addEventListener("keydown", (e) => {
+//       let rotation = this.el.getAttribute("rotation");
+//       let position = this.el.getAttribute("position");
+//       if (e.key == "ArrowUp") {
+//         if (rotation.z < 20) {
+//           // position.z -= 0.1;
+//           rotation.z -= 0.1;
+//         }
+//       }
+//       this.el.setAttribute("position", position);
+//       this.el.setAttribute("rotation", rotation);
+//     });
+//   },
+// });
+
+AFRAME.registerComponent("scuba-diver-rotation-reader", {
+  schema: {
+    speedOfRotation: {
+      type: "number",
+      default: 0,
+    },
+    moveY: {
+      type: "number",
+      default: 0,
+    },
+  },
+  init: function () {
+    window.addEventListener("keydown", (e) => {
+      this.data.speedOfRotation = this.el.getAttribute("rotation");
+      this.data.moveY = this.el.getAttribute("position");
+      if (e.key === "ArrowUp") {
+        if (this.data.speedOfRotation.z < 20) {
+          this.data.speedOfRotation.z += 0.5;
+        }
+        if (this.data.moveY.y < 2) {
+          this.data.moveY.y += 0.01;
+        }
+      }
+      if (e.key === "ArrowDown") {
+        if (this.data.speedOfRotation.z > -20) {
+          this.data.speedOfRotation.z -= 0.5;
+        }
+        if (this.data.moveY.y > -2) {
+          this.data.moveY.y -= 0.01;
+        }
+      }
+      if (e.key === "ArrowRight") {
+        if (this.data.speedOfRotation.x < 10) {
+          this.data.speedOfRotation.x += 0.5;
+        }
+      }
+      if (e.key === "ArrowLeft") {
+        if (this.data.speedOfRotation.x > -10) {
+          this.data.speedOfRotation.x -= 0.5;
+        }
+      }
+      this.el.setAttribute("rotation", this.data.speedOfRotation);
+      this.el.setAttribute("position", this.data.moveY);
+    });
+  },
+});
